8c317e5002489987ce0645b5f2b7d71c2bf1354a
trkim <qwert12a@naver.com>
2016-11-11 18:21:06 +0900

    콜백함수 추가

--- SisasServer/routes/index.js
+++ SisasServer/routes/index.js
@@ -92,17 +92,25 @@ router.post('/login',function(req,res){
   /*var input_email = 'aa@aa.com';
   var input_pwd =  '1111';*/
 
-  var member = Member.find({'email' : req.body.email, 'password' : req.body.password});
+  Member.find({'email' : req.body.email, 'password' : req.body.password}, function(err, member){
+    if(err){
+      return res.json({'result' : 'fail'});
+    }
+    if(member) {
+      console.log(member);
 
-  console.log(member);
-  if(isEmpty(member) || isNull(member)){
-    console.log('로그인 실패!');
-    return res.json({'result': 'login_failed'});
+      if (isEmpty(member) || isNull(member)) {
+        console.log('로그인 실패!');
+        return res.json({'result': 'login_failed'});
 
-  }else{
-    console.log("로그인 성공!");
-    return res.json(member);
-  }
+      } else {
+        console.log("로그인 성공!");
+        return res.json(member);
+      }
+    }
+  });
+
+
 });
 
 module.exports = router;
