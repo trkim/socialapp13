21e1ee4249d2b5ec3071f170f6ecc2c3ea5c0fe5
trkim <qwert12a@naver.com>
2016-11-13 15:27:00 +0900

    a

--- SisasServer/routes/index.js
+++ SisasServer/routes/index.js
@@ -76,11 +76,11 @@ router.post('/insert_member',function(req,res){
           return;
         }
         console.log("회원가입 완료");
-        res.status(200).send("inserted");
+        //res.status(200).send("inserted");
       });
     }else{
       console.log('이미 있는 email')
-      res.json({'result':'already_used'});
+      //res.json({'result':'already_used'});
     }
 
   })
