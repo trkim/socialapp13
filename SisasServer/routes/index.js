f31c777082c52d7b991900b2d4bce33ca39cddbf
trkim <qwert12a@naver.com>
2016-11-24 00:01:19 +0900

    클로저 제거

--- SisasServer/routes/index.js
+++ SisasServer/routes/index.js
@@ -351,9 +351,6 @@ router.get('/get_myroomlist', function(req,res){
   });
 });
 
-var room_id_list;
-var roomlist = [];
-
 router.get('/get_ctgroomlist', function(req,res){
   var category = req.query.category;
   if(category == 'all'){
@@ -377,26 +374,29 @@ router.get('/get_ctgroomlist', function(req,res){
       if(err){
         console.error(err);
         res.json({'result':'fail'});
-      }else {
+      }else{
         console.log('get roomlist 성공');
-        room_id_list = room_id;
-        for (var i = 0; i < room_id_list.length; i++) {
-          roomlist[i] = Room.findOne({'room_id': room_id_list[i]}, function (err, room) {
-            if (err) {
-              console.error(err);
 
-            }
-            if (room) {
-              console.log('index : ' + room_id_list[i]);
-              console.log(room);
-              return room;
-            }
-          });
+        for(var i=0;i<room_id.length;i++){
+          (function(m) {
+            var roomlist = [];
+            roomlist[m] = Room.findOne({'room_id': room_id[m]}, function (err, room) {
+                if (err) {
+                  console.error(err);
+                  res.json({'result': 'fail'});
+                }
+                if (room) {
+                  console.log('###' + m);
+                  console.log('$$$$$$$$$$$' + room)
+                  return room;
+                }
+              })
+          })(i);
         }
+        console.log('전체 roomlist :'+roomlist);
+        res.json(roomlist);
       }
     });
-    console.log('!@#@@@#!@#!@#!@#!@#!@3roomlist : '+roomlist);
-    res.json(roomlist);
   }else {
     Room.find({'category': category}, function (err, roomlist) {
       if (err) {
