e6bc0f1ef36d99a2ebe3e756c54be076231fd770
trkim <qwert12a@naver.com>
2016-12-17 16:50:44 +0900

    파이썬 코드 수정

--- SisasServer/public/pythonscripts/article_crawling.py
+++ SisasServer/public/pythonscripts/article_crawling.py
@@ -13,36 +13,32 @@ a = urllib.parse.quote(a)
 
 def get_url_with_keyword(URL, keyword):
   source_code_from_URL = URL+keyword
-  print(source_code_from_URL)
   return source_code_from_URL
 
 def get_title(URL):
-  print('get_title')
   soup = BeautifulSoup(requests.get(URL).text, 'lxml')
   text = ''
   for div in soup.find_all("a", class_="f_l"):
     text = text + str(div.find_all(text=True))
     sleep(2)
-  return text
+  print('get_title : '+text)
 
 
 def get_text(URL):
-  print('get_text')
   soup = BeautifulSoup(requests.get(URL).text, 'lxml')
   text = ''
   for item in soup.find_all('p', class_='f_eb'):
     text = text + str(item.find_all(text=True))
     sleep(2)
-  return text
+  print('get_text : '+text)
 
 def get_news_url(URL):
-  print('get_news_url')
   soup = BeautifulSoup(requests.get(URL).text, 'lxml')
   text = ''
   for link in soup.find_all('a', class_='f_l'):
     text = text + str(link.get('href'))
     sleep(2)
-  return text
+  print('get_news_url : '+text)
 
 
 def main(input_kwd):
@@ -54,16 +50,11 @@ def main(input_kwd):
     result_text = get_text(result_url)
     result_news_url = get_news_url(result_url)
 
-    print('python코드에서 출력')
     print(result_title)
     print(result_text)
     print(result_news_url)
 
-    return {
-        'result_title' : result_title,
-        'result_text' : result_text,
-        'result_news_url' : result_news_url
-    }
+
 
 
 
--- SisasServer/public/pythonscripts/run.py
+++ SisasServer/public/pythonscripts/run.py
@@ -2,5 +2,4 @@ import article_crawling
 import sys
 
 keyword = sys.argv[1]
-print(keyword)
 article_crawling.main(keyword)
