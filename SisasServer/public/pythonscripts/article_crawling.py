c52d6ae3c5383cdd5075c99026684005883230c5
trkim <qwert12a@naver.com>
2016-12-17 20:26:38 +0900

    크롤링 코드 한굴 출력됨

--- SisasServer/public/pythonscripts/article_crawling.py
+++ SisasServer/public/pythonscripts/article_crawling.py
@@ -1,58 +1,62 @@
 #-*- coding: utf-8 -*-
 import sys
 sys.path.insert(0,"../usr/local/lib/python3.5/site-packages/")
+#reload(sys)
+#sys.setdefaultencoding('utf-8')
 from bs4 import BeautifulSoup
 import urllib.parse
 import requests
 from time import sleep
 
-URL = 'http://search.daum.net/search?w=tot&DA=23A&rtmaxcoll=NNS&q='
+URL = 'http://search.daum.net/search?w=news&nil_search=btn&DA=NTB&enc=utf8&cluster=y&cluster_page=1&q='
 s = "사드"
 a = s.encode("utf-8")
 a = urllib.parse.quote(a)
 
+objarray = []
+
+class Obj:
+    title = ""
+    content = ""
+    url = ""
+
+
+
 def get_url_with_keyword(URL, keyword):
   source_code_from_URL = URL+keyword
+  print(source_code_from_URL)
   return source_code_from_URL
 
-def get_title(URL):
+def crawling(URL):
+  obj = Obj()
+  count =0
   soup = BeautifulSoup(requests.get(URL).text, 'lxml')
-  text = ''
-  for div in soup.find_all("a", class_="f_l"):
-    text = text + str(div.find_all(text=True))
+  title = ''
+  content = ''
+  contentlist = soup.find_all('p', class_='f_eb')
+  url = ''
+  for i in soup.find_all("a", class_="f_l"):
+    title = str(i.find_all(text=True))
+    print(title)
+    content = str(contentlist[count].find_all(text=True))
+    print(content)
+    url = str(i.get('href'))
+    print(url)
+    obj.title = title
+    obj.content = content
+    obj.url = url
+    objarray.append(obj)
     sleep(2)
-  print('get_title : '+text)
-
-
-def get_text(URL):
-  soup = BeautifulSoup(requests.get(URL).text, 'lxml')
-  text = ''
-  for item in soup.find_all('p', class_='f_eb'):
-    text = text + str(item.find_all(text=True))
-    sleep(2)
-  print('get_text : '+text)
-
-def get_news_url(URL):
-  soup = BeautifulSoup(requests.get(URL).text, 'lxml')
-  text = ''
-  for link in soup.find_all('a', class_='f_l'):
-    text = text + str(link.get('href'))
-    sleep(2)
-  print('get_news_url : '+text)
+    count = count+1
+  print(objarray)
 
 
 def main(input_kwd):
-    keyword = input_kwd.encode("utf-8")
+    keyword = input_kwd
     keyword = urllib.parse.quote(keyword)
 
     result_url = get_url_with_keyword(URL, keyword)
-    result_title = get_title(result_url)
-    result_text = get_text(result_url)
-    result_news_url = get_news_url(result_url)
-
-    print(result_title)
-    print(result_text)
-    print(result_news_url)
+    crawling(result_url)
 
 
 
