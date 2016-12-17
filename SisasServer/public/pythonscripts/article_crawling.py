680f846ae542c13f01dd7632b5a37fdb741dccbf
trkim <qwert12a@naver.com>
2016-12-18 00:08:36 +0900

    python 한글로 출력되게 바꿈

--- SisasServer/public/pythonscripts/article_crawling.py
+++ SisasServer/public/pythonscripts/article_crawling.py
@@ -5,38 +5,29 @@ from bs4 import BeautifulSoup
 import urllib.parse
 import requests
 from time import sleep
-import json
 
 URL = 'http://search.daum.net/search?w=news&nil_search=btn&DA=NTB&enc=utf8&cluster=y&cluster_page=1&q='
 s = "사드"
 a = s.encode("utf-8")
 a = urllib.parse.quote(a)
 
-
-
 objarray = []
-
 
 def __str__(self):
     return 'title=%s, content=%s, url=%s'%(self.title, self.content, self.url)
-
-def obj_dict(obj):
-    return obj.__dict__
 
 def get_url_with_keyword(URL, keyword):
   source_code_from_URL = URL+keyword
   return source_code_from_URL
 
-
 def crawling(URL):
   count =0
-
   soup = BeautifulSoup(requests.get(URL).text, 'lxml')
   title = ''
   content = ''
   contentlist = soup.find_all('p', class_='f_eb')
   url = ''
-  for i in soup.find_all("a", class_="f_link_bu"):
+  for i in soup.find_all("a", class_="f_l"):
     obj = {}
     title = str(i.find_all(text=True))
     content = str(contentlist[count].find_all(text=True))
@@ -44,11 +35,10 @@ def crawling(URL):
     obj['title'] = title
     obj['content'] = content
     obj['url'] = url
+    objarray.append(obj)
     sleep(2)
     count = count+1
-    objarray.append(obj)
-  return json.dumps(objarray)
-
+  print(objarray)
 
 
 def main(input_kwd):
@@ -56,8 +46,7 @@ def main(input_kwd):
     keyword = urllib.parse.quote(keyword)
 
     result_url = get_url_with_keyword(URL, keyword)
-    resultarray = crawling(result_url)
-    print(resultarray)
+    crawling(result_url)
 
 
 
@@ -65,4 +54,4 @@ def main(input_kwd):
 
 
 if __name__ == '__main__':
-  main(s)
+  main(a)
