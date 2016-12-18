3ec7b8f5dd098653ab2fc63e82c5e2ee085fce23
trkim <qwert12a@naver.com>
2016-12-19 02:32:45 +0900

    check_coupon 수정

--- SisasServer/routes/index.js
+++ SisasServer/routes/index.js
@@ -486,6 +486,7 @@ router.post('/check_coupon', function(req, res){
     }
     if((member.coupon*1) > 1){
       console.log('관전 가능. 잔여 쿠폰 : '+member.coupon);
+      console.log('타입 : '+typeof(member.coupon));
       member.coupon = ((member.coupon*1) - 1)+'';
       console.log('잔여 쿠폰 : '+member.coupon);
       member.save(function(err){
